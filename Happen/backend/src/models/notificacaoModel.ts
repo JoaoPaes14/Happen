e8985bb909ce12808f1cb36c46417bc1282d12b8
JoaoPaes14 <joaopaes556@gmail.com>
2024-12-27 16:43:41 -0300

    🐛 fix: Ajustando a configuração de data para os campos 'criado_em' e 'atualizado_em' no modelo de notificação

--- Happen/backend/src/models/notificacaoModel.ts
+++ Happen/backend/src/models/notificacaoModel.ts
@@ -1,51 +1,55 @@
 import { Model, DataTypes } from 'sequelize';
 import sequelize from '../config/database';
 
-class Notificacao extends Model{
+class Notificacao extends Model {
     public id!: number;
-    public id_usuario!:number;
-    public mensagem!:string;
+    public id_usuario!: number;
+    public mensagem!: string;
     public lida!: boolean;
     public criado_em!: Date;
+    public atualizado_em!: Date;
 }
 
 Notificacao.init(
     {
-        id:{
+        id: {
             type: DataTypes.INTEGER,
-            autoIncrement:true,
-            primaryKey:true,
+            autoIncrement: true,
+            primaryKey: true,
         },
-        id_usuario:{
-            type:DataTypes.INTEGER,
-            allowNull:false,
+        id_usuario: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
         },
-        mensagem:{
-            type:DataTypes.TEXT,
-            allowNull:false,
+        mensagem: {
+            type: DataTypes.TEXT,
+            allowNull: false,
         },
-        lida:{
-            type:DataTypes.BOOLEAN,
-            defaultValue:true,
+        lida: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true,
         },
         criado_em: { 
             type: DataTypes.DATE,
             allowNull: false,
-            defaultValue: () => new Date(), 
-          },
+            defaultValue: DataTypes.NOW,  
+        },
+        atualizado_em: { 
+            type: DataTypes.DATE, 
+            allowNull: false,
+            defaultValue: DataTypes.NOW,  
+        },
     },
     {
         sequelize,
         tableName: 'Notificacoes',  
         timestamps: false, 
         hooks: {
-            beforeUpdate: (usuario: any) => {
-      
-              usuario.atualizado_em = new Date();
-            }
-          }    
+            beforeUpdate: (notificacao: any) => {
+                notificacao.atualizado_em = new Date();  
+            },
+        },
     }
 );
-
 
 export default Notificacao;
