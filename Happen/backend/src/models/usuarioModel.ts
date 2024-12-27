b0d4679d134b6444b97d6843f7768661396a3e69
JoaoPaes14 <joaopaes556@gmail.com>
2024-12-27 15:57:59 -0300

    :sparkles: feat: Criando modelo de usuário

---
+++ Happen/backend/src/models/usuarioModel.ts
@@ -0,0 +1,68 @@
+import { Model, DataTypes } from 'sequelize';
+import sequelize from '../config/database';
+
+class Usuario extends Model {
+  public id!: number;
+  public nome!: string;
+  public email!: string;
+  public senha!: string;
+  public tipo!: 'comum' | 'organizador';
+  public foto_perfil!: string | null;
+  public criado_em!: Date;
+  public atualizado_em!: Date;
+}
+
+Usuario.init(
+  {
+    id: { 
+      type: DataTypes.INTEGER, 
+      primaryKey: true, 
+      autoIncrement: true 
+    },
+    nome: { 
+      type: DataTypes.STRING(100), 
+      allowNull: false 
+    },
+    email: { 
+      type: DataTypes.STRING(100), 
+      allowNull: false, 
+      unique: true 
+    },
+    senha: { 
+      type: DataTypes.STRING(255), 
+      allowNull: false 
+    },
+    tipo: { 
+      type: DataTypes.ENUM('comum', 'organizador'), 
+      allowNull: false 
+    },
+    foto_perfil: { 
+      type: DataTypes.STRING(255), 
+      allowNull: true 
+    },
+    criado_em: { 
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: () => new Date(), 
+    },
+    atualizado_em: { 
+      type: DataTypes.DATE, 
+      allowNull: false,
+      defaultValue: () => new Date(), 
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Usuario',
+    tableName: 'Usuarios',
+    timestamps: false, 
+    hooks: {
+      beforeUpdate: (usuario: any) => {
+
+        usuario.atualizado_em = new Date();
+      }
+    }
+  }
+);
+
+export default Usuario;
