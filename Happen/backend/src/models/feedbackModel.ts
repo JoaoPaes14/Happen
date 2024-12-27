1f03be25ec335045104efb04e64012d18970b506
JoaoPaes14 <joaopaes556@gmail.com>
2024-12-27 16:45:39 -0300

    🐛 fix: Ajustando a configuração de data para os campos 'criado_em' e 'atualizado_em' no modelo de feedback

--- Happen/backend/src/models/feedbackModel.ts
+++ Happen/backend/src/models/feedbackModel.ts
@@ -1,58 +1,64 @@
 import { Model, DataTypes } from 'sequelize';
 import sequelize from '../config/database';
 
-class Feedback extends Model{
+class Feedback extends Model {
     public id!: number;
-    public id_evento!:number;
+    public id_evento!: number;
     public id_usuario!: number;
-    public nota!:number;
+    public nota!: number;
     public comentario!: string;
     public criado_em!: Date;
+    public atualizado_em!: Date;  // Adicionando o campo 'atualizado_em'
 }
 
 Feedback.init(
     {
-        id:{
+        id: {
             type: DataTypes.INTEGER,
             autoIncrement: true,
-            primaryKey:true,
+            primaryKey: true,
         },
-        id_evento:{
+        id_evento: {
             type: DataTypes.INTEGER,
-            allowNull:false,
+            allowNull: false,
         },
-        id_usuario:{
+        id_usuario: {
             type: DataTypes.INTEGER,
-            allowNull:false,
+            allowNull: false,
         },
-        nota:{
-            type:DataTypes.INTEGER,
-            allowNull:false,
-            validate:{
-                min:1,
-                max:5,
+        nota: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 1,
+                max: 5,
             },
         },
-        comentario:{
+        comentario: {
             type: DataTypes.TEXT,
-            allowNull:true,
+            allowNull: true,
         },
         criado_em: { 
             type: DataTypes.DATE,
             allowNull: false,
-            defaultValue: () => new Date(), 
-          },
+            defaultValue: DataTypes.NOW,  
+        },
+        atualizado_em: {  
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
     },
     {
         sequelize,
         tableName: 'Feedback',  
         timestamps: false, 
         hooks: {
-            beforeUpdate: (usuario: any) => {
-      
-              usuario.atualizado_em = new Date();
+            beforeUpdate: (feedback: any) => {
+                feedback.atualizado_em = new Date(); 
             }
-          }    
+        }
     }
-)
+);
+
 export default Feedback;
