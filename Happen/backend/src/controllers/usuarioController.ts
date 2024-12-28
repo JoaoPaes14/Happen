d8bda10f79007760800c68bbbffd421a60b3370f
JoaoPaes14 <joaopaes556@gmail.com>
2024-12-28 14:14:22 -0300

    :sparkles: feat: Implementado as funções de  obter usuário no controller

--- Happen/backend/src/controllers/usuarioController.ts
+++ Happen/backend/src/controllers/usuarioController.ts
@@ -31,3 +31,18 @@ export const criarUsuario = async(req: Request, res:Response):Promise<Response> 
     }
 };
 
+export const obterUsuario = async (req: Request, res: Response): Promise<Response> => {
+    const { id } = req.params;
+
+    try {
+        const usuario = await Usuario.findByPk(id);
+
+        if (!usuario) {
+            return res.status(404).json({ message: 'Usuário não encontrado' });
+        }
+
+        return res.status(200).json(usuario);
+    } catch (error) {
+        return res.status(500).json({ message: 'Erro ao obter usuário', error });
+    }
+};
