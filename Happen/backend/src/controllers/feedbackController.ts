5ea7f2f3f62e18c5f93f4e4dd3259d086f64f788
JoaoPaes14 <joaopaes556@gmail.com>
2025-01-02 14:20:11 -0300

    :sparkles: feat: Implementado as função de listar  feedback no controller

--- Happen/backend/src/controllers/feedbackController.ts
+++ Happen/backend/src/controllers/feedbackController.ts
@@ -1,5 +1,6 @@
 import { Request,Response } from "express";
 import Feedback from "../models/feedbackModel";
+import { json } from "body-parser";
 
 export const criarFeedback = async (req:Request, res:Response):Promise<void> =>{
     const{usuarioId,eventoId,comentario,nota}= req.body;
@@ -16,5 +17,15 @@ export const criarFeedback = async (req:Request, res:Response):Promise<void> =>{
 
     } catch (error) {
         res.status(500).json({message: 'Erro ao criar feedback',error, });
+    }
+};
+
+export const listarFeedback = async(req:Request, res:Response):Promise<void> =>{
+    try{
+        const feedback = await Feedback.findAll();
+        res.status(200).json(feedback);
+    }catch(error){
+        res.status(500).json({message:'Erro ao listar feedbacks',error});
+
     }
 };
