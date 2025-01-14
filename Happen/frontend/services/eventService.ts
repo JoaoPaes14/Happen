96867e420798cb2fb0441667645a60b8f44b0995
JoaoPaes14 <joaopaes556@gmail.com>
2025-01-14 11:17:03 -0300

    :sparkles: feat: Adicionando no serviço de evento a função de obter evento

--- Happen/frontend/services/eventService.ts
+++ Happen/frontend/services/eventService.ts
@@ -3,19 +3,37 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const API_URL = 'http://192.168.252.247:3001/api/eventos';
 
-export const criarEvento = async (nome: string, descricao: string, local: string, data_hora: string, id_organizador: number) => {
+const getToken = async (): Promise<string | null> => {
+    return await AsyncStorage.getItem('token');
+  };
+
+  export const criarEvento = async (nome: string, descricao: string, local: string, data_hora: string, id_organizador: number) => {
     try {
-      const token = await AsyncStorage.getItem('token');
-      const response = await axios.post(`${API_URL}/criarEvento`, { nome, descricao, local, data_hora, id_organizador },
+      const token = await getToken();
+      const response = await axios.post(`${API_URL}/criarEvento`,{ nome, descricao, local, data_hora, id_organizador },
         {
           headers: {
             Authorization: `Bearer ${token}`,
           },
         }
       );
-  
       return response.data;
     } catch (error: any) {
       throw new Error(error.response?.data?.message || 'Erro ao criar evento');
+    }
+  };
+
+  export const obterEvento = async(id:number)=>{
+    try{
+        const token = await getToken();
+        const response = await axios.get(`${API_URL}/${id}`,{
+            headers:{
+                Authorization: `Bearrer ${token}`,
+            },
+        });
+        return response.data;
+    }catch(error:any){
+        throw new Error(error.response?.data?.message || 'Erro ao obter evento');
+
     }
   };
