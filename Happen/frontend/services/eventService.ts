144aab5dc9780cd715206358379367af5b2d70ff
JoaoPaes14 <joaopaes556@gmail.com>
2025-01-22 13:35:03 -0300

    :recycle: refactor: Mudanças no service de criar evento no frontend

--- Happen/frontend/services/eventService.ts
+++ Happen/frontend/services/eventService.ts
@@ -8,26 +8,42 @@ const getToken = async (): Promise<string | null> => {
   return await AsyncStorage.getItem('token');
 };
 
-export const criarEvento = async ( nome: string,descricao: string,local: string,data_hora: string,id_organizador: number) => {
+export const criarEvento = async ( nome: string,descricao: string,local: string,data_hora: string,id_organizador: number,imagem : any) => {
   try {
     const token = await getToken();
     if (!token) throw new Error('Token não encontrado');
-    const eventoData = { nome, descricao, local, data_hora, id_organizador };
+    
+    const formData = new FormData();
+    formData.append('nome', nome);
+    formData.append('descricao', descricao);
+    formData.append('local', local);
+    formData.append('data_hora', data_hora);
+    formData.append('id_organizador', String(id_organizador));
+    
+    
+    if (imagem) {
+      
+      const image = {
+        uri: imagem.uri,  
+        type: imagem.type || 'image/jpeg', 
+        name: imagem.name || 'imagem.jpg',  
+      };
+      
+     
+      formData.append('imagem', image as any); 
+    }
 
-    const response = await axios.post(`${API_URL}/criarEvento`,eventoData,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
+    const response = await axios.post(`${API_URL}/criarEvento`, formData, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'multipart/form-data', 
+      },
+    });
 
     return response.data;
   } catch (error: any) {
-   
     console.error('Erro detalhado:', error);
     if (error.response) {
-      
       console.error('Erro do servidor:', error.response.data);
     }
     throw new Error(error.response?.data?.message || 'Erro ao criar evento');
@@ -65,7 +81,7 @@ export const listarEventos = async (nome?: string, data_inicial?: string, data_f
     if (data_inicial) params.data_inicial = data_inicial;
     if (data_final) params.data_final = data_final;
 
-    console.log('Parâmetros da requisição:', params); // Depuração
+    console.log('Parâmetros da requisição:', params); 
 
     const response = await axios.get(`${API_URL}/listarEventos`, {
       headers: {
@@ -74,7 +90,7 @@ export const listarEventos = async (nome?: string, data_inicial?: string, data_f
       params,
     });
 
-    console.log('Resposta da API:', response.data); // Depuração
+    console.log('Resposta da API:', response.data); 
 
     return response.data.eventos;
   } catch (error: any) {
